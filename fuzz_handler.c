a9fcb2448d406dec5a9afff08467143127885c35
soier <soier@home>
2019-06-23 22:36:19 +0500

    added tcp/udp wrap in fuzz_handler

--- fuzz_handler.c
+++ fuzz_handler.c
@@ -3,8 +3,23 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <unistd.h>
 
-void fuzz(char *buf, unsigned int len)
+static void tcp(void* buf, unsigned int len)
+{
+	int s;
+	struct sockaddr_in addr;
+
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(445);
+	connect(s, (struct sockaddr *)&addr, sizeof(addr));
+	send(s, buf, len, 0);
+	close(s);
+}
+
+static void udp(char *buf, unsigned int len)
 {
 	int s;
 	struct sockaddr_in addr;
@@ -12,7 +27,13 @@ void fuzz(char *buf, unsigned int len)
 	s = socket(AF_INET, SOCK_DGRAM, 0);
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	addr.sin_port = htons(68);
+	addr.sin_port = htons(137);
 	sendto(s, buf, len, 0, (struct sockaddr *)&addr, sizeof(addr));
 	close(s);
+}
+
+void* fuzz(char *buf, unsigned int len)
+{
+	udp(buf, len);
+	//tcp(buf, len);
 }
